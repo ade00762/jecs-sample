17165a22d8be9941b3db661623a4f6605da06b2d
ade00762 <56198705+ade00762@users.noreply.github.com>
2024-04-11 14:55:46 +0100

    1st commit
    ok

---
+++ js/index.js
@@ -0,0 +1,22 @@
+function sendMail(){
+    var params={
+        name:document.getElementById("name").value ,
+        email:document.getElementById("email").value ,
+        message:document.getElementById("message").value ,
+    }
+    const serviceID = "service_k3bhkcp";
+const templateID = "template_mmu8d8w"
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("message").value="";
+        console.log(res);
+        alert("your message sent successfully")
+    }
+)
+.catch ((err)=> console.log (err));
+};
+
